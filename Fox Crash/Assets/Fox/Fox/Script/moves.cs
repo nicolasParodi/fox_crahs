f9b6716c516203588363e2d61e103039ad86d607
PC-07\PC01 <nachoagus@live.com.ar>
2016-10-04 12:24:19 -0300

    LALALA

--- Fox Crash/Assets/Fox/Fox/Script/moves.cs
+++ Fox Crash/Assets/Fox/Fox/Script/moves.cs
@@ -1,9 +1,56 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
 
 public class moves : MonoBehaviour {
+    Animator Player;
+    void Start()
+    {
+        Player = GetComponent<Animator>();
+    }    
+   
+    void Update () {
+        ConsoleKeyInfo Keys;
+        //do
+        //{
+            Keys = Console.ReadKey();
 
-	void Update () {
-	    
-	}
+            switch (Keys.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    Player.Play("Jump");
+                    break;
+            }
+        //} while ();
+
+
+
+       /*if()
+        {
+            Player.Play("Jump");
+        }
+        if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            Player.Play("Couch");
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            Player.Play("Walk");
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            do {
+                Player.Play("Run");
+            }while()
+            
+        }
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            Player.Play("Idle");
+        }*/
+    }
 }
